431254fb5a5a84c7ff7fd64bbfdbe29242565eff
sofien6 <foufou.hedjazi@gmail.com>
2022-05-03 17:16:42 +0100

    add user

--- src/component/login/login.jsx
+++ src/component/login/login.jsx
@@ -1,8 +1,55 @@
-import React from 'react'
-
+import React , {useState} from 'react'
+import './css/design.css'
+import {Form , Button} from 'react-bootstrap'
+import { async } from '@firebase/util'
+import LoginDataServices from '../../services/login.services'
 const Login = () => {
+
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+  const [chekmeout, setchekmeout] = useState("")
+
+  const creatUser = async (e) => {
+      e.preventDefault(); 
+      const myUser = {
+        email,
+        password,
+        chekmeout
+      }
+
+      try{
+        const user = await LoginDataServices.addusers(myUser)
+        alert(' user added  succesfuly  ^^  ')
+        setemail("")
+        setpassword("")
+      }catch(error){
+        alert(' Error   ^^  ')
+      }
+  }
+
   return (
-    <div>Login</div>
+    <div className='login_parent'>
+      <Form onSubmit={creatUser}>
+  <Form.Group className="mb-3" controlId="formBasicEmail">
+    <Form.Label style={{color:"white"}} onChange={(e)=>setemail(e.target.value)} value={email} >Email address</Form.Label>
+    <Form.Control type="email" placeholder="Enter email" />
+    <Form.Text style={{color:"white"}} className="text-muted">
+      We'll never share your email with anyone else.
+    </Form.Text>
+  </Form.Group>
+
+  <Form.Group className="mb-3" controlId="formBasicPassword">
+    <Form.Label style={{color:"white"}} onChange={(e)=>setpassword(e.target.value)} value={password}>Password</Form.Label>
+    <Form.Control type="password" placeholder="Password" />
+  </Form.Group>
+  <Form.Group style={{color:"white"}} className="mb-3" controlId="formBasicCheckbox">
+    <Form.Check onChange={(e)=>setchekmeout(e.target.value)} value={chekmeout} type="checkbox" label="Check me out" />
+  </Form.Group>
+    <Button  variant="primary" type="submit">
+      Submit
+    </Button>
+    </Form>
+    </div>
   )
 }
 
---
+++ src/services/login.services.jsx
@@ -0,0 +1,31 @@
+import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, updateDoc } from "firebase/firestore";
+import { db } from "../firebase-config";
+
+
+const userCollectionRef = collection(db,"users")
+
+class LoginDataServices {
+    addusers = (newuser) =>{
+       return addDoc(userCollectionRef,newuser)
+    }
+
+    updateusers = (id,updatedusers) => {
+        const validateuser = doc(db,'users',id); 
+        return updateDoc(validateuser,updatedusers)
+    }
+
+    deleteuser = (id, deletedusers) =>{
+        const deletedValidatuserd = doc(db,'users',id)
+        return deleteDoc(deletedValidatuserd,deletedusers)
+    }
+
+    getAllusers = ()=>{
+        return getDocs(userCollectionRef)
+    }
+    getuser = (id)=>{
+        const getuser = doc(db,'users',id);
+        return getDoc(getuser,id);
+    }
+}
+
+export default new LoginDataServices();
